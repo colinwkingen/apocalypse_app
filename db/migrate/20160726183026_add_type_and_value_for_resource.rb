76ec323904394bb7757a3fd9d0e681bd19a28481
Colin W Kingen and Matt Carlson <matt@>
2016-07-26 11:47:47 -0700

    change schema to add item type and value

---
+++ db/migrate/20160726183026_add_type_and_value_for_resource.rb
@@ -0,0 +1,6 @@
+class AddTypeAndValueForResource < ActiveRecord::Migration
+  def change
+    add_column(:resources, :item_type, :string)
+    add_column(:resources, :value, :int)
+  end
+end
--- lib/resource.rb
+++ lib/resource.rb
@@ -3,11 +3,11 @@ class Resource < ActiveRecord::Base
   has_many :users, through: :amounts
 
   define_singleton_method(:populate_items) do
-    Resource.create({name: "Beans", cost: 1.29, incrementable: true, unit: "Can"})
-    Resource.create({name: "Black Cherry Fresca", cost: 1.79, incrementable: true, unit: "Can"})
-    Resource.create({name: "Vicodin", cost: 5.00, incrementable: true, unit: "Pill"})
-    Resource.create({name: "Jar of Pickles", cost: 3.59, incrementable: true, unit: "Jar"})
-    Resource.create({name: "Life Jacket", cost: 20.00, incrementable: false, unit: "Jacket"})
-    Resource.create({name: "Rubber Gloves", cost: 2.59, incrementable: true, unit: "Pair"})
+    Resource.create({name: "Beans", cost: 1.29, incrementable: true, unit: "Can", value: 2, item_type: "Food"})
+    Resource.create({name: "Black Cherry Fresca", cost: 1.79, incrementable: true, unit: "Can", value: 2, item_type: "Water"})
+    Resource.create({name: "Vicodin", cost: 5.00, incrementable: true, unit: "Pill", value: 2, item_type: "Protection"})
+    Resource.create({name: "Jar of Pickles", cost: 3.59, incrementable: true, unit: "Jar", value: 2, item_type: "Food"})
+    Resource.create({name: "Life Jacket", cost: 20.00, incrementable: false, unit: "Jacket", value: 2, item_type: "Protection"})
+    Resource.create({name: "Rubber Gloves", cost: 2.59, incrementable: true, unit: "Pair", value: 2, item_type: "Protection"})
   end
 end
