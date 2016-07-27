fe8a6c5b588e6c9c35b25a24ce84e870cb2038ce
Patrick Lipscomb <patrick@>
2016-07-27 09:09:08 -0700

    add the probability functions to the disaster class.

--- lib/disaster.rb
+++ lib/disaster.rb
@@ -1,6 +1,7 @@
 class Disaster < ActiveRecord::Base
 
   define_method(:every_day) do |user|
+    message = ''
     if user.alive == false
       break
     end
@@ -23,6 +24,9 @@ class Disaster < ActiveRecord::Base
         user.update({medicine_count: (medicine_current - 6)})
         user.update({protection_count: (protection_current - 6)})
       end
+      if self.gas_mask?(user) && self.name == 'Contagion'
+        user.update({alive: false})
+      end
     end
     if self.name == 'Nuclear'
       user.update({food_count: (food_current - 12)})
@@ -30,6 +34,9 @@ class Disaster < ActiveRecord::Base
       if rand(3) > 1
         user.update({medicine_count: (medicine_current - 6)})
         user.update({protection_count: (protection_current - 9)})
+      end
+      if self.hazmat_suit?(user) && self.name == 'Nuclear'
+        user.update({alive: false})
       end
     end
     if (user.food_count.to_i < 0) || (user.water_count.to_i < 0)
@@ -39,5 +46,41 @@ class Disaster < ActiveRecord::Base
     else
       user.update({high_score: (user.high_score.to_i + 10)})
     end
+  end
+
+  define_method(:gas_mask?) do |user|
+    resource_names = []
+    chance_of_contagion = false
+    user.resources.each() do |resource|
+      resource_names.push(resource.name)
+    end
+    if resource_names.include?("Gas Mask")
+      if rand(20) > 18
+        chance_of_contagion = true
+      end
+    else
+      if rand(20) > 9
+        chance_of_contagion = true
+      end
+    end
+    chance_of_contagion
+  end
+
+  define_method(:hazmat_suit?) do |user|
+    resource_names = []
+    chance_of_radiation = false
+    user.resources.each() do |resource|
+      resource_names.push(resource.name)
+    end
+    if resource_names.include?("Hazmat Suit")
+      if rand(20) > 18
+        chance_of_radiation = true
+      end
+    else
+      if rand(20) > 9
+        chance_of_radiation = true
+      end
+    end
+    chance_of_radiation
   end
 end
