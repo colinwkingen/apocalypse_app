11c04087b1215317bdfdadb5cdec47f8a65b3dfe
Colin W Kingen <colinwkingen@gmail.com>
2016-07-28 15:36:52 -0700

    resolve merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727200948) do
+ActiveRecord::Schema.define(version: 20160728213207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160727200948) do
     t.integer "medicine_count"
     t.integer "protection_count"
     t.boolean "alive"
+    t.boolean "event_flag"
   end
 
 end
